180714609af461b8cce5aa8047478d9a6a986e0b
Tobias Bradtke <webwurst@gmail.com>
2018-11-29 14:22:17 +0100

    Add error handling

--- process1.py
+++ process1.py
@@ -1,25 +1,14 @@
-
 import os
 import json
 from elasticsearch import Elasticsearch
 
-# print(os.environ['HOME'])
-# print(os.environ['ELASTICSEARCH_INDEX_URL'])
 
-# es = Elasticsearch('http://localhost:9200/')
-
-host, index = os.environ['ELASTICSEARCH_INDEX_URL_AGENT'].rsplit('/', 1)
+host, index_agent = os.environ['ELASTICSEARCH_INDEX_URL_AGENT'].rsplit('/', 1)
 _, index_processing = os.environ['ELASTICSEARCH_INDEX_URL_PROCESSING'].rsplit('/', 1)
 es = Elasticsearch([host])
 
-# res = es.index(index=index, doc_type='_doc', id=1, body=doc)
-# print(res['result'])
-#
-# res = es.get(index=index, doc_type='_doc', id=1)
-# print(res['_source'])
 
-
-res = es.search(index=index, body={"query": {"match_all": {}}})
+res = es.search(index=index_agent, body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total'])
 
 for hit in res['hits']['hits'][0:1]:
@@ -27,10 +16,8 @@ for hit in res['hits']['hits'][0:1]:
         # print(json.dumps(item, indent=2))
         print(item["kind"])
 
-        res = es.index(index=index_processing, doc_type='_doc', id=1, body=item)
-        print(res['result'])
-
-
-# todo
-# dest-index
-# item["_reporting_meta"] = { customer, installation, node, }
+        try:
+          res = es.index(index=index_processing, doc_type='_doc', id=1, body=item)
+          print(res['result'])
+        except elasticsearch.ElasticsearchException as e:
+          print str(e)
