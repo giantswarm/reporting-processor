1dc02cfc2b2d0942fec774982d28b64bfb046828
Tobias Bradtke <webwurst@gmail.com>
2018-11-29 15:27:03 +0100

    Use uid from resource as first approach to tackle duplication

--- process1.py
+++ process1.py
@@ -2,7 +2,6 @@ import os
 import json
 import elasticsearch
 
-
 host, index_agent = os.environ['ELASTICSEARCH_INDEX_URL_AGENT'].rsplit('/', 1)
 _, index_processing = os.environ['ELASTICSEARCH_INDEX_URL_PROCESSING'].rsplit('/', 1)
 es = elasticsearch.Elasticsearch([host])
@@ -16,7 +15,8 @@ for hit in res['hits']['hits'][0:1]:
         # print(json.dumps(item, indent=2))
         print(item["kind"])
         try:
-            res = es.index(index=index_processing, doc_type='_doc', id=1, body=item)
+            uid = item["metadata"]["uid"]
+            res = es.index(index=index_processing, doc_type='_doc', id=uid, body=item)
             print(res['result'])
         except elasticsearch.ElasticsearchException as e:
             print(str(e))
