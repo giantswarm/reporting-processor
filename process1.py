601a245fa7dc62d58712c37d9cc26bb0e3b5182a
Fernando Ripoll <pipo02mix@gmail.com>
2019-01-11 15:54:03 +0100

    Process item by item

--- process1.py
+++ process1.py
@@ -10,13 +10,12 @@ es = elasticsearch.Elasticsearch([host])
 res = es.search(index=index_agent, body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total'])
 
-for hit in res['hits']['hits'][0:1]:
-    for item in hit["_source"]["items"]:
-        # print(json.dumps(item, indent=2))
-        print(item["kind"])
-        try:
-            uid = item["metadata"]["uid"]
-            res = es.index(index=index_processing, doc_type='_doc', id=uid, body=item)
-            print(res['result'])
-        except elasticsearch.ElasticsearchException as e:
-            print(str(e))
+for doc in res['hits']['hits']:
+  item = doc["_source"]
+  if 'kind' in item:
+    try:
+      uid = item["metadata"]["uid"]
+      res = es.index(index=index_processing, doc_type='_doc', id=item['metadata']['uid'], body=item)
+      print(res['result'])
+    except elasticsearch.ElasticsearchException as e:
+      print(str(e))
